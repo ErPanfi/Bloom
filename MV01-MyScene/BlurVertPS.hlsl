03ee4dd9a81c2582220c8729e99ccd6285eae987
Daniele Colombo (ElvenInside) <secret.mail@gmail.com>
2013-05-21 15:33:33 +0200

    blur

--- MV01-MyScene/BlurVertPS.hlsl
+++ MV01-MyScene/BlurVertPS.hlsl
@@ -12,19 +12,26 @@ struct PixelShaderInput
     float2 texCoord : TEXCOORD0;
 };
 
-static const float blurVertSize = 0.0025f;
+//static const float blurVertSize = 0.0025f;
 static const float blurVertWeights[] = {0.16f, 0.15f, 0.12f, 0.09f, 0.05f};
 
 float4 main(PixelShaderInput input) : SV_TARGET
 {
+
+	float screenHeight = 480.0f;
+	float texelSize = 1.0f / screenHeight;
+
+
 	float3 rgb = float3(0.0, 0.0, 0.0);
 	float blurMaxWeight = 0.16f;
 
 	[unroll(15)] for(int i = 0; i < level; i++)
 	{
 		//vertical blur
-		rgb += inputTexture.Sample(texSampler, float2(input.texCoord.x, input.texCoord.y - (i * blurVertSize))).rgb * (blurMaxWeight - i * 0.01f);
-		rgb += inputTexture.Sample(texSampler, float2(input.texCoord.x, input.texCoord.y + (i * blurVertSize))).rgb * (blurMaxWeight - i * 0.01f);
+		//rgb += inputTexture.Sample(texSampler, float2(input.texCoord.x, input.texCoord.y - (i * blurVertSize))).rgb * (blurMaxWeight - i * 0.01f);
+		//rgb += inputTexture.Sample(texSampler, float2(input.texCoord.x, input.texCoord.y + (i * blurVertSize))).rgb * (blurMaxWeight - i * 0.01f);
+		rgb += inputTexture.Sample(texSampler, input.texCoord + float2(0.0f, (-i * texelSize))).rgb * (blurMaxWeight - i * 0.01f);
+		rgb += inputTexture.Sample(texSampler, input.texCoord + float2(0.0f, (+i * texelSize))).rgb * (blurMaxWeight - i * 0.01f);
 	}
 
 	return float4(rgb, 1.0f);
